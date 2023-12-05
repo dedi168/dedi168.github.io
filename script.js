8a5992aa2c82b5bdffba3110bcf77868ffa90565
dedi suardana <56145508+dedi168@users.noreply.github.com>
2023-12-05 08:04:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,4 @@ yesBtn.addEventListener("click", () => {
   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; 
 });
 
-noBtn.addEventListener("mouseover", () => {
 
-const noBtnRect = noBtn.getBoundingClientRect();  
-
-const maxY = window.innerHeight - noBtnRect.height;
-
-const randomX = Math.floor(Math.random() * maxX);
-
-const randomY = Math.floor(Math.random() * maxY);
-
-noBtn.style.left = randomX + "px"; 
-noBtn.style.top = randomY + "px"; 
-});
