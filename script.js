b99ff77cf07730d55d334289b33732df70233b0a
dedi suardana <56145508+dedi168@users.noreply.github.com>
2023-12-05 08:03:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,7 @@ yesBtn.addEventListener("click", () => {
 
 noBtn.addEventListener("mouseover", () => {
 
-const noBtnRect = noBtn.getBoundingClientRect(); 
-
-const maxX = window.innerWidth - noBtnRect.width;
+const noBtnRect = noBtn.getBoundingClientRect();  
 
 const maxY = window.innerHeight - noBtnRect.height;
 
